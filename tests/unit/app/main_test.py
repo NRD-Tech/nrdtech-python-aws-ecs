dcd60637d7e95482da04afe550911a79b2b693d2
Nic Delorme <delormen@gmail.com>
2025-06-16 16:35:02 -0500

    updated tests

--- tests/unit/app/main_test.py
+++ tests/unit/app/main_test.py
@@ -1,3 +1,7 @@
+##########################################
+# Basic App Test
+##########################################
+
 from app.main import main
 from dotenv import load_dotenv
 
@@ -6,3 +10,21 @@ load_dotenv()
 
 def test_main():
     main()
+
+##########################################
+# FastAPI App Test
+##########################################
+
+# from fastapi.testclient import TestClient
+# from app.main import app
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# client = TestClient(app)
+
+
+# def test_ping():
+#     response = client.get("/ping")
+#     assert response.status_code == 200
+#     assert response.json() == {"message": "pong"}
